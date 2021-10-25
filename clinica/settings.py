2fa6950cdfa00cd98c8a8b34a875ecd1fbbdf9bd
AzhaelVS <azhaelvallarta@gmail.com>
2021-10-25 13:14:46 -0500

    deploy

--- clinica/settings.py
+++ clinica/settings.py
@@ -10,10 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
+import os
+from decouple import config
 from pathlib import Path
+from dj_database_url import parse as dburl
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,6 +35,10 @@ ALLOWED_HOSTS = []
 ALLOWED_ORIGINS = ['http://localhost']
 
 # Application definition
+
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -78,12 +88,9 @@ WSGI_APPLICATION = 'clinica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+default_dburl='sqlite:///'+os.path.join(BASE_DIR,'db.sqlite3')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': config('DATABASE_URL',default=default_dburl, cast=dburl),}
 
 
 # Password validation
@@ -123,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
