edb8a308ac74abd48e24b6073d8b344cf971a6ce
AzhaelVS <azhaelvallarta@gmail.com>
2021-10-25 13:45:02 -0500

    deploy statics v2

--- clinica/settings.py
+++ clinica/settings.py
@@ -25,13 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-twsw#diq%#&1ozh!h1=qw1v%j!0e_uuf6s*xa-5mpxk@=e_y+v'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 ALLOWED_ORIGINS = ['http://localhost']
 
 # Application definition
@@ -130,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
